33c74aaedfa4b865f294c1500c732f66e3e2e30c
Ramūnas Genzuras <45263750+GreenNBlacko@users.noreply.github.com>
2024-09-10 01:00:12 +0300

    Remove unnecessary files

--- PD1/IO/ConsoleOutput.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace PD1.IO {
-    public static class ConsoleOutput {
-        public static void SendMessage(string Message, bool FullLine = false) {
-            if (FullLine) Console.WriteLine(Message);
-            else Console.Write(Message);
-        }
-    }
-}
--- PD3/IO/FileInput.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Drawing;
-
-namespace PD3.IO {
-    public static partial class FileIO {
-        public static string ReadFile(string Path) => File.ReadAllText(path: Path);
-
-        public static Bitmap ReadImage(string Path) => new Bitmap(filename: Path);
-    }
-}
